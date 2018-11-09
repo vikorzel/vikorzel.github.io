aae5c5c5c90565d1bdff358538dddcc4ba4e9c83
Travis CI User <travis@example.org>
2018-11-09 17:59:13 +0000

    Travis AutoCommit

--- search/classes_1.js
+++ search/classes_1.js
@@ -6,5 +6,5 @@ var searchData=
   ['is_5fcontainer',['is_container',['../structis__container.html',1,'']]],
   ['is_5fsame_5ftypes',['is_same_types',['../structis__same__types.html',1,'']]],
   ['is_5fsame_5ftypes_3c_20t_2c_20u_20_3e',['is_same_types&lt; T, U &gt;',['../structis__same__types_3_01T_00_01U_01_4.html',1,'']]],
-  ['is_5fsame_5ftypes_3c_20t_2c_20u_2c_20types_2e_2e_2e_20_3e',['is_same_types&lt; T, U, Types... &gt;',['../structis__same__types_3_01T_00_01U_00_01Types_8_8_8_01_4.html',1,'']]]
+  ['is_5fsame_5ftypes_3c_20t_2c_20u_2c_20types_2e_2e_2e_3e',['is_same_types&lt; T, U, Types...&gt;',['../structis__same__types_3_01T_00_01U_00_01Types_8_8_8_4.html',1,'']]]
 ];
